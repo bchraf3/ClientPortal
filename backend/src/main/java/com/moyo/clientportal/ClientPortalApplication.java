6ea9bd4740fdea023986d10f1463554d6a422d36
Bouchaib Chraf <135054721+bchraf3@users.noreply.github.com>
2025-01-28 16:29:00 +0200

    feat(backend): Initialize Spring Boot project structure and API setup

---
+++ backend/src/main/java/com/moyo/clientportal/ClientPortalApplication.java
@@ -0,0 +1,13 @@
+package com.moyo.clientportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientPortalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/moyo/clientportal/ClientPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyo.clientportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
